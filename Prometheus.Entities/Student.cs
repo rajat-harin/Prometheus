ef997c1363e4eb4abbba318da2f72e77d57c62ca
rajat-harin <rajat.harinkhede@gmail.com>
2021-03-26 17:43:29 +0530

    Created Layers and added student class

---
+++ Prometheus.Entities/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prometheus.Entities
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+        public string City { get; set; }
+        public string MobileNo { get; set; }
+
+
+    }
+}
