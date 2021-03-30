e6d7df99225358af7750dbb18db5fedfc40e1a8e
rajat-harin <rajat.harinkhede@gmail.com>
2021-03-30 11:37:12 +0530

    resolving gitignore error

--- Prometheus.Entities/Student.cs
+++ Prometheus.Entities/Student.cs
@@ -12,6 +12,7 @@ namespace Prometheus.Entities
         public int StudentID { get; set; }
         public string FName { get; set; }
         public string LName { get; set; }
+        public string UserName { get; set; }
         public string Address { get; set; }
         public DateTime DOB { get; set; }
         public string City { get; set; }
@@ -22,6 +23,7 @@ namespace Prometheus.Entities
             StudentID = 0;
             FName = String.Empty;
             LName = String.Empty;
+            UserName = String.Empty;
             Address = String.Empty;
             DOB = DateTime.Now;
             City = String.Empty;
